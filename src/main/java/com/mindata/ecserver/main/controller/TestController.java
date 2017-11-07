bc2857bba3c9da7fd0e427644bdb926fd483efb9
wuwf <272551766@qq.com>
2017-11-07 15:43:32 +0800

    加个controller

---
+++ src/main/java/com/mindata/ecserver/main/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.mindata.ecserver.main.controller;
+
+import com.mindata.ecserver.main.BaseData;
+import com.mindata.ecserver.main.service.FetchCompanyPhoneHistoryService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+
+/**
+ * @author wuweifeng wrote on 2017/11/7.
+ */
+@RestController
+public class TestController {
+    @Resource
+    private FetchCompanyPhoneHistoryService fetchCompanyPhoneHistoryService;
+
+    @GetMapping("/fetch")
+    public BaseData fetchCompanyHistory() throws IOException {
+        return fetchCompanyPhoneHistoryService.fetch();
+    }
+}
