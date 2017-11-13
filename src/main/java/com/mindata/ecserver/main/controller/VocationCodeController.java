f172d285fb14382c087567a0222d05fa84e5f48a
hanliqianggithub <hanliqiang@dingtalk.com>
2017-11-13 19:09:33 +0800

    匹配返回code

--- src/main/java/com/mindata/ecserver/main/controller/VocationCodeController.java
+++ src/main/java/com/mindata/ecserver/main/controller/VocationCodeController.java
@@ -3,19 +3,19 @@ package com.mindata.ecserver.main.controller;
 import com.mindata.ecserver.global.bean.BaseData;
 import com.mindata.ecserver.global.bean.ResultGenerator;
 import com.mindata.ecserver.main.manager.EsVocationCodeManager;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
 @RestController
-@RequestMapping("/code")
+@RequestMapping("/vocationCode")
 public class VocationCodeController {
     @Resource
     private EsVocationCodeManager esVocationCodeManager;
 
-    @PostMapping({"/returnCode"})
+    @GetMapping({""})
     public BaseData queryContactData(String vocationName) {
         return ResultGenerator.genSuccessResult(esVocationCodeManager.findByRequestBody(vocationName));
     }
