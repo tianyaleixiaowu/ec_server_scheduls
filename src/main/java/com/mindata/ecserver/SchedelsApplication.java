5c32ff827996b0a6839cfa5bc7dc44a4041fab84
wuwf <272551766@qq.com>
2017-11-24 16:37:54 +0800

    加上eureka

--- src/main/java/com/mindata/ecserver/SchedelsApplication.java
+++ src/main/java/com/mindata/ecserver/SchedelsApplication.java
@@ -2,10 +2,12 @@ package com.mindata.ecserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableDiscoveryClient
 public class SchedelsApplication {
 
     public static void main(String[] args) {
