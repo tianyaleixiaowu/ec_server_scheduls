1fe5c350ca8d1195b86db8c80e16274470e705d1
wuwf <272551766@qq.com>
2017-11-25 09:59:27 +0800

    动态决定是否启动eureka

--- src/main/java/com/mindata/ecserver/SchedelsApplication.java
+++ src/main/java/com/mindata/ecserver/SchedelsApplication.java
@@ -2,12 +2,10 @@ package com.mindata.ecserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableDiscoveryClient
 public class SchedelsApplication {
 
     public static void main(String[] args) {
