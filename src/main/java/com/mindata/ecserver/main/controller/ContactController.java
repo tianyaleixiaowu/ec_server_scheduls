60e829dc76ab5dba3428f5a3ec8dbaa5992af2d6
wuwf <272551766@qq.com>
2017-12-06 16:46:10 +0800

    提供删除eccontact的接口

--- src/main/java/com/mindata/ecserver/main/controller/ContactController.java
+++ src/main/java/com/mindata/ecserver/main/controller/ContactController.java
@@ -1,6 +1,7 @@
 package com.mindata.ecserver.main.controller;
 
 import com.mindata.ecserver.main.manager.ContactManager;
+import com.mindata.ecserver.main.service.EsContactService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,14 +10,26 @@ import javax.annotation.Resource;
 /**
  * @author wuweifeng wrote on 2017/11/21.
  */
+@RequestMapping("/contact")
 @RestController
 public class ContactController {
     @Resource
     private ContactManager contactManager;
+    @Resource
+    private EsContactService esContactService;
 
     @RequestMapping("/completeProvince")
     public Object completeProvince() {
         contactManager.completeAreaCode();
         return "补齐完毕";
+    }
+
+    /**
+     * 删除Contact
+     */
+    @RequestMapping("/delete")
+    public Object deleteContact(String ids) {
+        esContactService.deleteByIds(ids);
+        return contactManager.deleteByIds(ids);
     }
 }
