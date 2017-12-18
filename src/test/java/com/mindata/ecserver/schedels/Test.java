b1be4e957de838928d1c9530c5babf9a8289be1e
hanliqianggithub <hanliqiang@dingtalk.com>
2017-12-18 13:57:08 +0800

    coordinate

--- src/test/java/com/mindata/ecserver/schedels/Test.java
+++ src/test/java/com/mindata/ecserver/schedels/Test.java
@@ -5,8 +5,6 @@ package com.mindata.ecserver.schedels;
  */
 public class Test {
     public static void main(String[] args){
-        String address = "天津市东丽区滨海机场外院9#-1-105-106号";
-        String aa = address.replace("#","");
-        System.out.println(aa);
+
     }
 }
